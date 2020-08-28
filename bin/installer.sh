aaf47b41233aa0e5f6448b21e8ae291cb493d061
Yuto Chikazawa <chikazawa0517@gmail.com>
2020-08-28 16:21:22 +0900

    Update installer

--- bin/installer.sh
+++ bin/installer.sh
@@ -4,7 +4,13 @@ echo "##### setup tmux #####"
 
 if [ -z $(which tmux) ]; then
   echo "----- install tmux -----"
-  brew install tmux
+  if [ "$(uname)" == 'Darwin' ]; then
+    # mac
+    brew install tmux
+  elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    # linux
+    yum install tmux
+  fi
 fi
 
 echo "----- link tmux.conf -----"
