9cf1ac5c7b7ac318399dab6a2d7f3b6e0c8431aa
Yuto Chikazawa <chikazawa0517@gmail.com>
2021-12-07 01:14:25 +0900

    Add gh & lazygit

--- git/bin/installer.sh
+++ git/bin/installer.sh
@@ -5,8 +5,26 @@ if [ -z $(which git) ]; then
   brew install git
 fi
 
+# create .config dir
+if [ ! -d ~/.config ]; then
+  echo "create ~/.config directory"
+  mkdir ~/.config
+fi
+
 # link git settings
-for file in "$(pwd)/git/.config/git"/*
+LINK_FILES=(.config/git)
+for file in ${LINK_FILES[@]}
 do
-  ln -sf $file ~/.config/git
+  unlink ~/$file&>/dev/null
+  ln -sf $(pwd)/git/$file ~/$file
+done
+
+# install other CLI
+CLI_LIST=(gh lazygit)
+for cli in ${CLI_LIST[@]}
+do
+  if [ -z $(which $cli) ]
+  then
+    brew install $cli
+  fi
 done
